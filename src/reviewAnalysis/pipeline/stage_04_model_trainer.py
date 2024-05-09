7b3f161796c5555962892a7f3a0379f7d90280bb
Subhayu Dutta <duttasuvo90@gmail.com>
2024-05-09 19:07:34 +0530

    Fourth

--- src/reviewAnalysis/pipeline/stage_04_model_trainer.py
+++ src/reviewAnalysis/pipeline/stage_04_model_trainer.py
@@ -19,7 +19,7 @@ STAGE_NAME = "Model Trainer stage"
 if __name__ == '__main__':
     try:
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-        obj = ModelTrainer()
+        obj = ModelTrainerTrainingPipeline()
         obj.main()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
     except Exception as e:
