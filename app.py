494536603d6aa06dbd2cb85d1fc83b897a2eb4ff
Subhayu Dutta <duttasuvo90@gmail.com>
2024-05-11 12:09:53 +0530

    port added

--- app.py
+++ app.py
@@ -30,5 +30,5 @@ def predict_route():
         return str(e)
 
 if __name__ == "__main__":
-    #app.run(host="0.0.0.0", port=8080)
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=8080)
+    #app.run(debug=True)
